64370d0dfa8b4293809c64e28d23b80852844f6b
Anubhav311 <anubhavbhambri1@gmail.com>
2019-05-14 04:22:34 +0530

    Refactored code in enhancer.js

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -7,35 +7,29 @@ module.exports = {
 
 function succeed(item) {
   (item.enhancement == 20 ? item : item.enhancement++);
+  let newItem = Object.assign({}, item)
+  // return newItem
   return { ...item };
 }
 
-function fail(item) {
+function fail (item) {
   if(item.enhancement < 15) {
-    if(item.durability < 5) {
-      item.durability = 0;
-    } else {
-      item.durability = item.durability - 5;
-    }
+    item.durability < 5 ? item.durability = 0 : item.durability = item.durability - 5;
   } else if (item.enhancement == 15 || item.enhancement == 16) {
-    if(item.durability < 10) {
-      item.durability = 0;
-    } else {
-      item.durability = item.durability - 10;
-    }
+    item.durability < 10 ? item.durability = 0 : item.durability = item.durability - 10
   } else if (item.enhancement > 16) {
-    if(item.durability < 10) {
-      item.durability = 0;
-    } else {
-      item.durability = item.durability - 10;
-    }
+    item.durability < 10 ? item.durability = 0 : item.durability = item.durability - 10;
     item.enhancement--;
   }
+  let newItem = Object.assign({}, item)
+  // return newItem
   return { ...item };
 }
 
 function repair(item) {
   item.durability = 100;
+  let newItem = Object.assign({}, item)
+  // return newItem
   return { ...item };
 }
 

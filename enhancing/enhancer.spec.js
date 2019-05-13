37cfc0f79a6302fe9045969964c8c07db43a9ae6
Anubhav311 <anubhavbhambri1@gmail.com>
2019-05-14 02:27:40 +0530

    added comment for new branch

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -3,6 +3,8 @@ const enhancer = require('./enhancer.js');
 
 describe('enhancer', () => {
     describe('repair() method', () => {
+
+        //tests for repair method
         it('should restore durability to 100', () => {
             const expected = {
                 name: 'item_1',
@@ -10,24 +12,24 @@ describe('enhancer', () => {
                 enhancement: 20
             };
 
-            const actual_1 = enhancer.repair({
+            const actual_repair_1 = enhancer.repair({
                 name: 'item_1',
                 durability: 50,
                 enhancement: 20
             })
 
-            const actual_2 = enhancer.repair({
+            const actual_repair_2 = enhancer.repair({
                 name: 'random',
                 durability: 50,
                 enhancement: 15
             })
 
-            expect(expected.durability).toEqual(actual_1.durability);
-            expect(expected.durability).toEqual(actual_2.durability);
+            expect(expected.durability).toEqual(actual_repair_1.durability);
+            expect(expected.durability).toEqual(actual_repair_2.durability);
         });
 
         it('should increase enhancement by 1', () => {
-            
+
         })
     })
 })
